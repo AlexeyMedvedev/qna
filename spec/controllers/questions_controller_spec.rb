b5d3a89469429d35552d09f9b7c2fdf8150d85e3
Alexey Medvedev <rallysuperman@gmail.com>
2015-03-16 23:40:09 +0300

    question show tested

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,18 +2,39 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
-    it 'populate questions array' do
-      question1 = FactoryGirl.create(:question)
-      question2 = FactoryGirl.create(:question)
+  	let(:questions) {create_list(:question, 2)}
+  	before do
+      #@questions = FactoryGirl.create_list(:question, 2)
+      #@questions = create_list(:question, 2)
 
       get :index
+  	end
 
-      expect(assigns(:questions)).to match_array([question1, question2])
+    it 'populate questions array' do
+      #question1 = FactoryGirl.create(:question)
+      #question2 = FactoryGirl.create(:question)
+
+      #get :index
+
+      expect(assigns(:questions)).to match_array(questions)
     end
 
     it 'render index view' do
-      get :index
+      #get :index
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+  	let(:question) {create(:question)}
+    it 'set question to @question' do
+      get :show, id: question
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'render show view' do
+      get :show, id: question
+      expect(response).to render_template :show
     end
   end
 end
