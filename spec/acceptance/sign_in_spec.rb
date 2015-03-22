bcecbbf618dc942bef05f28f9bdda7d742a8b050
Alexey Medvedev <rallysuperman@gmail.com>
2015-03-22 13:29:26 +0300

    User sign-in added

---
+++ spec/acceptance/sign_in_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'User sign-in', %q{
+  In order be Authentified_User
+  as Guest_User
+  Guest_User able to sign-in
+}do
+  
+  scenario 'Registred user try to sign-in' do
+    User.create!(email: 'reguser@fake.ru', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'reguser@fake.ru'
+    fill_in 'Password', with: '12345678'
+    #save_and_open_page
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully.'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'Unregistred user try to sign-in' do
+    
+    visit new_user_session_path
+    fill_in 'Email', with: 'unreguser@fake.ru'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+
+    #save_and_open_page
+    expect(page).to have_content 'Invalid email or password.'
+    expect(current_path).to eq new_user_session_path
+  end
+end
