5374a16e5747789afbf6ee344016baa7523eba59
Alexey Medvedev <rallysuperman@gmail.com>
2015-03-24 11:46:57 +0300

    user association added

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ActiveRecord::Base
 	belongs_to :question
 	validates :text, presence: true
+
+  belongs_to :user
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
 	has_many :answers, dependent: :destroy
 	validates :topic, :text, presence: true
+
+  belongs_to :user
 end
