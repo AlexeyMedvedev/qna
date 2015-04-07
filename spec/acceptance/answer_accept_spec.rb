aaa0aa096a4bb25854376be2ba2c4456df5b5e2b
Alexey Medvedev <rallysuperman@gmail.com>
2015-04-07 23:53:01 +0300

    accept acceptance tests added

--- spec/acceptance/answer_accept_spec.rb
+++ spec/acceptance/answer_accept_spec.rb
@@ -9,7 +9,7 @@ feature 'Select accept', %q{
   given(:user) {create(:user)}
   given(:anuser){create(:user)}
   given(:question){ create(:question)}
-  given!(:answer){ create(:answer, question: question, user: user) }
+  given!(:answer){ create(:answer, question: question, user: user, text: 'Answer text') }
   
   scenario 'Author see best link', js: true do
     sign_in(user)
@@ -34,4 +34,38 @@ feature 'Select accept', %q{
       expect(page).to_not have_link 'Best'
     end
   end
+
+  scenario 'Selected best answer first on page', js: true do
+    answers = create_list(:answer, 2, user: user, question: question)
+   
+    sign_in(user)
+
+    visit question_path(question)
+    within "#answer-#{answer.id}" do
+      click_on 'Best'
+    end
+
+    first_answer_on_page = page.find('.answer:first-child')
+
+    expect(page).to have_selector('.answer', count: 3)
+    expect(first_answer_on_page).to have_content answer.text
+  end
+
+  scenario 'New selected best first on page', js: true do
+    answer2 = create(:answer, question: question, user: user)
+    answer3 = create(:answer, question: question, user: user, accepted: true, text: 'Old best answer')
+    
+    sign_in(user)
+    visit question_path(question)
+    within "#answer-#{answer.id}" do
+      click_on 'Best'
+    end
+
+    first_answer_on_page = page.find('.answer:first-child')
+
+    expect(first_answer_on_page).to have_content answer.text
+    expect(page.find('.answer-accept')).to_not have_content answer3.text
+       
+  end
+
 end
