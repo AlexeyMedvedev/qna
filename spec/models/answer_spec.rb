df46064b4a1d9cec7ea20226d6fed385ff928344
Alexey Medvedev <rallysuperman@gmail.com>
2015-04-07 21:47:35 +0300

    answer model tests added

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,4 +4,27 @@ RSpec.describe Answer, type: :model do
   it {should validate_presence_of(:text)}
   it {should belong_to(:question)}
   it {should belong_to(:user)}
+
+
+  describe 'accept' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:answer) { create(:answer, user: user, question: question) }
+
+    it 'should assign accept to true' do
+      answer.accept
+      expect(answer.accepted).to eq true
+    end
+
+    it 'should change accept to false' do
+      answer2 = create(:answer, user: user, question: question)
+      answer2.accept
+      answer.accept
+      answer.reload
+      answer2.reload
+      expect(answer.accepted).to eq true
+      expect(answer2.accepted).to eq false
+    end
+
+  end
 end
